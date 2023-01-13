0fac8c6bc97774febdd8e433930acbddc027683c
sol/ <119491303+JcFaderanga@users.noreply.github.com>
2023-01-13 21:25:42 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 
+/** NAVBAR WHEN SROLL **/
+  const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains("scroll-down")
+  ) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+  lastScroll = currentScroll;
+});
 
 /** HUMBERGUR BUTTON **/
 const burger=document.querySelector('.humber-container');
@@ -87,3 +110,4 @@ document.querySelector('.services').style.display="none";
 document.querySelector('.projects').style.display="none";
 document.querySelector('.contact').style.display="block";
 });
+
